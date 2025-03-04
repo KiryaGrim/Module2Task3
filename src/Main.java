20f7c9fcb1cd27213aebcbf2badf8226a6cb0543
Grim_Lord <kbebishev98@bk.ru>
2025-03-04 16:49:18 +0300

    Module2Task3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] arr = new int[20];
+        Random rand = new Random();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Заполняем массив случайными числами от 1 до 15 включительно
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(15) + 1;
+        }
+
+        // Выводим массив
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + (i < arr.length - 1 ? ", " : ""));
+        }
+        System.out.println("]");
+
+        // Подсчитываем количество вхождений в массив для каждого числа
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Выводим числа, которые встречаются более одного раза
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println("Число '" + entry.getKey() + "' встречается " + entry.getValue() + " раза");
+            }
         }
     }
 }
